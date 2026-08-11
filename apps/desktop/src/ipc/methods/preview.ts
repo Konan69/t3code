101413d60921b8d61848a43899745fd7891c43de
Konan <kixeyems0@gmail.com>
2026-08-11 08:38:59 +0100

    feat(preview): add cookie setting

--- apps/desktop/src/ipc/methods/preview.ts
+++ apps/desktop/src/ipc/methods/preview.ts
@@ -9,6 +9,7 @@ import {
   DesktopPreviewAutomationTypeInputSchema,
   DesktopPreviewAutomationWaitForInputSchema,
   DesktopPreviewConfigInputSchema,
+  DesktopPreviewSetCookieInputSchema,
   DesktopPreviewNavigateInputSchema,
   DesktopPreviewRecordingArtifactSchema,
   DesktopPreviewRecordingSaveInputSchema,
@@ -204,6 +205,16 @@ export const clearCookies = DesktopIpc.makeIpcMethod({
   }),
 });
 
+export const setCookie = DesktopIpc.makeIpcMethod({
+  channel: IpcChannels.PREVIEW_SET_COOKIE_CHANNEL,
+  payload: DesktopPreviewSetCookieInputSchema,
+  result: Schema.Void,
+  handler: Effect.fn("desktop.ipc.preview.setCookie")(function* ({ environmentId, cookie }) {
+    const manager = yield* PreviewManager.PreviewManager;
+    yield* manager.setCookie(environmentId, cookie);
+  }),
+});
+
 export const clearCache = DesktopIpc.makeIpcMethod({
   channel: IpcChannels.PREVIEW_CLEAR_CACHE_CHANNEL,
   payload: Schema.Void,
@@ -385,6 +396,7 @@ export const methods = [
   setAudioMuted,
   openDevTools,
   clearCookies,
+  setCookie,
   clearCache,
   getPreviewConfig,
   setAnnotationTheme,
